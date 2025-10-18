3c5970709bbeb972131da80b7453be80d0bdd24d
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2025-10-18 17:18:36 +0100

    feat: Add some post processing, lights and camera controls

--- Assets/Scripts/Components/CameraDragging.cs
+++ Assets/Scripts/Components/CameraDragging.cs
@@ -8,14 +8,19 @@ namespace Game.Components
         public float DragSpeed = 6;
         private Vector3 _dragOrigin;
         private InputAction _moveAxis;
+        private InputAction _zoom;
 
         void Start()
         {
             _moveAxis = InputSystem.actions.FindAction("Camera Movement");
+            _zoom = InputSystem.actions.FindAction("Camera Zoom");
         }
         
         void Update()
         {
+            var zoom = _zoom.ReadValue<float>();
+            Camera.main.orthographicSize = Mathf.Min(10, Mathf.Max(3, Camera.main.orthographicSize + -zoom * 6 * Time.deltaTime));
+            
             var movement = _moveAxis.ReadValue<Vector2>();
             if (movement.sqrMagnitude == 0)
             {

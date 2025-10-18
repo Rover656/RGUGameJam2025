787506ddb17d4dd8d5941f97b79fd718719b5219
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2025-10-18 23:52:06 +0100

    chore: Basic UI

--- Assets/Scripts/Components/BuildingManager.cs
+++ Assets/Scripts/Components/BuildingManager.cs
@@ -1,11 +1,8 @@
-using System.Collections.Generic;
-using Game.Resources;
 using UnityEngine;
 
 namespace Game.Components
 {
     public class BuildingManager : MonoBehaviour
     {
-        public List<Buildable> Buildables = new();
     }
 }

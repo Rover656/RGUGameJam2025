c5f54518855d64284e24b976f8dfbab2c9115e2d
Your Name <zavzoe@gmail.com>
2025-10-19 06:37:13 +0100

    zo late night working
    - failed at syncing animation
    - added money display
    - added some kind of level design
    - added script and button for deleting objects

---
+++ Assets/Scripts/DeleteComponent.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace Game
+{
+    public class DeleteComponent : MonoBehaviour
+    {
+
+        public GameObject ItemRendererPrefab;
+        //want to be able to select button and delete any placed item
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
